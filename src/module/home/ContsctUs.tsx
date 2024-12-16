58ff580bb515c58640ca1d45f96234d1dc4b1134
gurez <boxbrothersocial@gmail.com>
2024-12-16 10:57:58 +0530

    asdasd

--- src/module/home/ContsctUs.tsx
+++ src/module/home/ContsctUs.tsx
@@ -1,126 +1,186 @@
-'use client'
-import { motion } from 'framer-motion'
-import { Input } from '@/components/ui/input'
-import { Textarea } from '@/components/ui/textarea'
-import { Button } from '@/components/ui/button'
-import { Instagram, Linkedin, Mail, MapPin, Phone, ArrowRight } from 'lucide-react'
-import Link from 'next/link'
-import ContactForm from '../constact-us/contact-form'
-import { Card, CardContent } from '@/components/ui/card'
+"use client";
+import { motion } from "framer-motion";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Instagram,
+  Linkedin,
+  Mail,
+  MapPin,
+  Phone,
+  ArrowRight,
+  Loader2,
+} from "lucide-react";
+import Link from "next/link";
+import ContactForm from "../constact-us/contact-form";
+import { Card, CardContent } from "@/components/ui/card";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useAddSubscriberMutation } from "@/state/api";
+import { generate32BitUUID } from "@/lib/service/generate32BitUUID";
 export default function ContsctUs() {
-    const fadeIn = {
-        hidden: { opacity: 0, y: 20 },
-        visible: { opacity: 1, y: 0 }
+  const [addSubscriber, { isLoading }] = useAddSubscriberMutation();
+  const [email, setEmail] = useState<string>("");
+  const fadeIn = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+  };
+  const socialLinks = [
+    // { icon: Facebook, href: '#', label: 'Facebook' },
+    {
+      icon: Instagram,
+      href: "https://www.instagram.com/karnalwebtech",
+      label: "Instagram",
+    },
+    {
+      icon: Linkedin,
+      href: "https://www.linkedin.com/in/karnalwebtech",
+      label: "LinkedIn",
+    },
+    // { icon: Twitter, href: '#', label: 'Twitter' }
+  ];
+  const isValidEmail = (email: string): boolean => {
+    // This regex is more strict and will catch more invalid email formats
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    return emailRegex.test(email) && email.includes('.') && !email.endsWith('.')
+  }
+  const subscriberHandler = async () => {
+    if (!email || !email.trim()) {
+      toast.error("Email field should not be empty.");
+      return;
     }
-    const socialLinks = [
-        // { icon: Facebook, href: '#', label: 'Facebook' },
-        { icon: Instagram, href: 'https://www.instagram.com/karnalwebtech', label: 'Instagram' },
-        { icon: Linkedin, href: 'https://www.linkedin.com/in/karnalwebtech', label: 'LinkedIn' },
-        // { icon: Twitter, href: '#', label: 'Twitter' }
-    ]
-    return (
-        <section className="relative overflow-hidden">
-            <div className="container relative mx-auto px-6 py-12 md:py-24">
-                <div className="grid gap-12 lg:grid-cols-2 place-items-center">
-                    {/* Left Column */}
-                    <motion.div
-                        initial="hidden"
-                        whileInView="visible"
-                        viewport={{ once: true }}
-                        variants={{
-                            hidden: { opacity: 0, x: -20 },
-                            visible: { opacity: 1, x: 0 }
-                        }}
-                        transition={{ duration: 0.5 }}
-                        className="space-y-8"
-                    >
-                        <div className="space-y-4">
-                            <h2 className="text-3xl lg:text-3xl font-bold text-center lg:text-start tracking-tight md:text-5xl">
-                                Let&apos;s Work Together!
-                            </h2>
-                            <p className="text-muted-foreground text-sm">
-                                I would like to meet with you to discuss something and opportunities for collaboration.
-                            </p>
-                        </div>
-                        <div className="space-y-4">
-                            <div className="flex items-center space-x-3 text-muted-foreground text-sm">
-                                <Mail className="h-5 w-5 text-black" />
-                                <span>info@karnalwebtech.com</span>
-                            </div>
-                            <div className="flex items-center space-x-3 text-muted-foreground text-sm">
-                                <MapPin className="h-5 w-5 text-black" />
-                                <span>Sector-6, Karnal, Haryana 132001, India</span>
-                            </div>
-                            <div className="flex items-center space-x-3 text-muted-foreground text-sm">
-                                <Phone className="h-5 w-5 text-black" />
-                                <span><Link href={"tel:+918816041566"}>+91 88160 41566</Link></span>
-                            </div>
-                        </div>
-                    </motion.div>
-
-                    {/* Right Column - Contact Form */}
-                    <div className='p-4'>
-                        <Card>
-                            <CardContent className='mt-4'>
-                                <ContactForm />
-                            </CardContent>
-                        </Card>
-                    </div>
-                </div>
-
-                {/* Social Links */}
-                <motion.div
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true }}
-                    variants={{
-                        hidden: { opacity: 0, y: 20 },
-                        visible: { opacity: 1, y: 0 }
-                    }}
-                    transition={{ delay: 0.2, duration: 0.5 }}
-                    className="mt-12 flex justify-center space-x-6"
-                >
-                    {socialLinks.map((social, index) => (
-                        <motion.a
-                            key={social.label}
-                            href={social.href}
-                            variants={fadeIn}
-                            target="__blank"
-                            whileHover={{ scale: 1.1 }}
-                            className="rounded-full bg-black p-3 text-gray-300 transition-colors hover:bg-white/10 hover:text-black"
-                            aria-label={social.label}
-                        >
-                            <social.icon className="h-5 w-5" />
-                        </motion.a>
-                    ))}
-                </motion.div>
-
-                {/* Newsletter Section */}
-                <motion.div
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true }}
-                    variants={{
-                        hidden: { opacity: 0, y: 20 },
-                        visible: { opacity: 1, y: 0 }
-                    }}
-                    transition={{ delay: 0.4, duration: 0.5 }}
-                    className="mt-16 rounded-xl bg-black p-8 text-center"
-                >
-                    <h3 className="mb-2 text-xl font-semibold text-white">Get Notified About Project</h3>
-                    <p className="mb-6 text-gray-300">Subscribe Now</p>
-                    <div className="mx-auto flex max-w-md gap-2">
-                        <Input
-                            placeholder="Email"
-                            type="email"
-                            className="border-blue-500/30 bg-white/5 text-white placeholder:text-gray-400"
-                        />
-                        <Button size="icon" className="bg-white text-black hover:text-white hover:bg-gray-700">
-                            <ArrowRight className="h-4 w-4" />
-                        </Button>
-                    </div>
-                </motion.div>
+    if (!isValidEmail(email.trim())) {
+      toast.error("Please enter a valid email address.")
+      return
+    }
+    try {
+      await addSubscriber({ email: email.trim(), uuid: generate32BitUUID() });
+      toast.success("Thank you for subscribing! Stay tuned for exciting project updates coming your way.")
+      setEmail("")
+    } catch (error) {
+      console.error("Subscription error:", error);
+      toast.error("Something went wrong. Please try again.");
+    }
+  };
+  return (
+    <section className="relative overflow-hidden">
+      <div className="container relative mx-auto px-6 py-12 md:py-24">
+        <div className="grid gap-12 lg:grid-cols-2 place-items-center">
+          {/* Left Column */}
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            variants={{
+              hidden: { opacity: 0, x: -20 },
+              visible: { opacity: 1, x: 0 },
+            }}
+            transition={{ duration: 0.5 }}
+            className="space-y-8"
+          >
+            <div className="space-y-4">
+              <h2 className="text-3xl lg:text-3xl font-bold text-center lg:text-start tracking-tight md:text-5xl">
+                Let&apos;s Work Together!
+              </h2>
+              <p className="text-muted-foreground text-sm">
+                I would like to meet with you to discuss something and
+                opportunities for collaboration.
+              </p>
             </div>
-        </section>
-    )
+            <div className="space-y-4">
+              <div className="flex items-center space-x-3 text-muted-foreground text-sm">
+                <Mail className="h-5 w-5 text-black" />
+                <span>info@karnalwebtech.com</span>
+              </div>
+              <div className="flex items-center space-x-3 text-muted-foreground text-sm">
+                <MapPin className="h-5 w-5 text-black" />
+                <span>Sector-6, Karnal, Haryana 132001, India</span>
+              </div>
+              <div className="flex items-center space-x-3 text-muted-foreground text-sm">
+                <Phone className="h-5 w-5 text-black" />
+                <span>
+                  <Link href={"tel:+918816041566"}>+91 88160 41566</Link>
+                </span>
+              </div>
+            </div>
+          </motion.div>
+
+          {/* Right Column - Contact Form */}
+          <div className="p-4">
+            <Card>
+              <CardContent className="mt-4">
+                <ContactForm />
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+
+        {/* Social Links */}
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          variants={{
+            hidden: { opacity: 0, y: 20 },
+            visible: { opacity: 1, y: 0 },
+          }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          className="mt-12 flex justify-center space-x-6"
+        >
+          {socialLinks.map((social, index) => (
+            <motion.a
+              key={social.label}
+              href={social.href}
+              variants={fadeIn}
+              target="__blank"
+              whileHover={{ scale: 1.1 }}
+              className="rounded-full bg-black p-3 text-gray-300 transition-colors hover:bg-white/10 hover:text-black"
+              aria-label={social.label}
+            >
+              <social.icon className="h-5 w-5" />
+            </motion.a>
+          ))}
+        </motion.div>
+
+        {/* Newsletter Section */}
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          variants={{
+            hidden: { opacity: 0, y: 20 },
+            visible: { opacity: 1, y: 0 },
+          }}
+          transition={{ delay: 0.4, duration: 0.5 }}
+          className="mt-16 rounded-xl bg-black p-8 text-center"
+        >
+          <h3 className="mb-2 text-xl font-semibold text-white">
+            Get Notified About Project
+          </h3>
+          <p className="mb-6 text-gray-300">Subscribe Now</p>
+          <div className="mx-auto flex max-w-md gap-2">
+            <Input
+              placeholder="Email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="border-blue-500/30 bg-white/5 text-white placeholder:text-gray-400"
+            />
+            <Button
+              onClick={() => subscriberHandler()}
+              size="icon" disabled={isLoading}
+              className="bg-white text-black hover:text-white hover:bg-gray-700"
+            >
+              {isLoading ? (
+                <Loader2 className="h-4 w-4 animate-spin" />
+              ) : (
+                <ArrowRight className="h-4 w-4" />
+              )}
+
+            </Button>
+          </div>
+        </motion.div>
+      </div>
+    </section>
+  );
 }
--- src/module/layout/footer/Footer.tsx
+++ src/module/layout/footer/Footer.tsx
@@ -1,10 +1,40 @@
+"use client"
 import Link from "next/link"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
-import { Instagram, Linkedin } from "lucide-react"
+import { Instagram, Linkedin, Loader2 } from "lucide-react"
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useAddSubscriberMutation } from "@/state/api";
+import { generate32BitUUID } from "@/lib/service/generate32BitUUID";
 
 export default function Footer() {
+  const [addSubscriber, { isLoading }] = useAddSubscriberMutation();
+  const [email, setEmail] = useState<string>("");
+  const isValidEmail = (email: string): boolean => {
+    // This regex is more strict and will catch more invalid email formats
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    return emailRegex.test(email) && email.includes('.') && !email.endsWith('.')
+  }
+  const subscriberHandler = async () => {
+    if (!email || !email.trim()) {
+      toast.error("Email field should not be empty.");
+      return;
+    }
+    if (!isValidEmail(email.trim())) {
+      toast.error("Please enter a valid email address.")
+      return
+    }
+    try {
+      await addSubscriber({ email: email.trim(), uuid: generate32BitUUID() });
+      setEmail("")
+      toast.success("Thank you for subscribing! Stay tuned for exciting project updates coming your way.")
+    } catch (error) {
+      console.error("Subscription error:", error);
+      toast.error("Something went wrong. Please try again.");
+    }
+  };
   return (
     <footer className="bg-background">
       <div className="container px-4 py-10 mx-auto">
@@ -12,7 +42,7 @@ export default function Footer() {
           <div className="space-y-4">
             <h3 className="text-lg font-semibold">Karnalwebtech</h3>
             <p className="text-sm text-muted-foreground">
-            Crafting cutting-edge web solutions to bring your ideas to life. Let’s build something extraordinary together!
+              Crafting cutting-edge web solutions to bring your ideas to life. Let’s build something extraordinary together!
             </p>
           </div>
           <div className="space-y-4">
@@ -28,17 +58,31 @@ export default function Footer() {
           </div>
           <div className="space-y-4">
             <h3 className="text-lg font-semibold">Stay Updated</h3>
-            <form className="space-y-2">
-              <Input type="email" placeholder="Enter your email" />
-              <Button  className="w-full bg-black hover:text-black">Subscribe</Button>
-            </form>
+            <div>
+
+              <Input placeholder="Email"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="border-blue-500/30 mb-2 bg-white/5 text-black placeholder:text-gray-400" />
+              <Button className="w-full bg-black hover:text-gray-200" onClick={() => subscriberHandler()}
+                disabled={isLoading}
+              >
+                {isLoading ? (
+                  <>
+                    <Loader2 className="h-4 w-4 animate-spin" /> <span>Subscribe...</span>
+                  </>
+                ) : (
+                  "Subscribe"
+                )}</Button>
+            </div>
           </div>
         </div>
         <Separator className="my-8" />
         <div className="flex flex-col items-center justify-between gap-4 lg:flex-row">
           <p className="text-sm text-muted-foreground">© 2023 Karnalwebtech. All rights reserved.</p>
           <div className="flex space-x-4">
-            <Link target="__blank"  href="https://www.linkedin.com/in/karnalwebtech" className="text-muted-foreground hover:text-foreground transition-colors">
+            <Link target="__blank" href="https://www.linkedin.com/in/karnalwebtech" className="text-muted-foreground hover:text-foreground transition-colors">
               <Linkedin className="w-5 h-5" />
               <span className="sr-only">LinkedIn</span>
             </Link>
